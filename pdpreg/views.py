2a5278e2d481b3ed519fc7ed77558fc3a6b047f4
Julien Eid <jeid64@gmail.com>
2013-08-11 09:23:25 -0400

    Work.

--- pdpreg/views.py
+++ pdpreg/views.py
@@ -1,6 +1,15 @@
 from pyramid.view import view_config
 
 
+@view_config(route_name='addevent', renderer='json')
+def addevent(request):
+#	cshUserName = request.headers["X-Webauth-User"]
+	name = request.params['name']
+	starttime = request.params['starttime']
+	endtime = request.params['endtime']
+	date = request.params['date']
+	return {'username' : 123, 'userNumber' : 0}
+
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'pdpreg'}
